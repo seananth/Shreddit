8ce66bb30a18cc931a7265780a7f5fc748bde220
seananth <seansafstrom@gmail.com>
2020-10-02 19:40:12 +0200

    added comments model and association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     validates :title, :URL, presence: true
 
+    has_many :comments, dependent: :destroy
     belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     validates :username, length: {minimum: 3}
     validates :username, length: {maximum: 12}
 
-    has_many :posts
+    has_many :posts, dependent: :destroy
+    has_many :comments
 end
